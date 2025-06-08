8cd9cbdf32ad4e19724d9d0c56ff79fc341884e4
Shams M. Monem <shamsmmonem@gmail.com>
2025-06-08 16:01:57 +0300

    simulation works

--- src/dtm_jtag.sv
+++ src/dtm_jtag.sv
@@ -40,96 +40,83 @@ always_ff @(negedge tclk, negedge trst)
         tdo <= 0;
         instruction <= IDCODE;
     end else begin
-        tdo <= next_tdo;
-
-        if (next_state == UPDATE_IR)
+        if(state == SHIFT_DR)
+            case(instruction)
+                IDCODE: begin 
+                    tdo <= dr[0];
+                end
+                BYPASS: begin 
+                    tdo <= bypass;
+                end
+            endcase
+        else if (state == SHIFT_IR)
+            tdo <= ir[0];
+        else if (state == UPDATE_IR)
             instruction <= ir;
     end
 
-
 always_ff @(posedge tclk, negedge trst)
     if (!trst) begin
+        state <= TEST_LOGIC_RESET;
         dr <= IDCODE_VALUE;
-        next_tdo <= 0;
         bypass <= 0;
-    end else begin
-        case (next_state)
+    end else
+        case(state)
             TEST_LOGIC_RESET: begin
+                state <= tms ? TEST_LOGIC_RESET : RUN_TEST_IDLE;            
                 dr <= IDCODE_VALUE;
-                next_tdo <= 0;
                 bypass <= 0;
             end
-            CAPTURE_IR: ir <= 6'b0000_01;
-            SHIFT_IR: begin
-                next_tdo <= ir[0];
-                ir <= {tdi, ir[5:1]};
-            end  
+            RUN_TEST_IDLE: 
+                state <= tms ? SELECT_DR_SCAN : RUN_TEST_IDLE;            
+            SELECT_DR_SCAN: 
+                state <= tms ? SELECT_IR_SCAN : CAPTURE_DR;         
             CAPTURE_DR: begin
+                state <= tms ? EXIT1_DR : SHIFT_DR;     
                 case(instruction)
                     IDCODE: dr <= IDCODE_VALUE;
                 endcase
             end
             SHIFT_DR: begin
+                state <= tms ? EXIT1_DR : SHIFT_DR;
+
                 case(instruction)
                     IDCODE: begin 
-                        next_tdo <= dr[0];
                         dr <= {tdi, dr[31:1]};
                     end
                     BYPASS: begin 
-                        next_tdo <= bypass;
                         bypass <= tdi;
                     end
                 endcase
             end
-            UPDATE_DR: begin
+            EXIT1_DR: 
+                state <= tms ? UPDATE_DR : PAUSE_DR;            
+            PAUSE_DR: 
+                state <= tms ? EXIT2_DR : PAUSE_DR;
+            EXIT2_DR: 
+                state <= tms ? UPDATE_DR : SHIFT_DR;
+            UPDATE_DR: 
+                state <= tms ? SELECT_DR_SCAN : RUN_TEST_IDLE;
+            SELECT_IR_SCAN: 
+                state <= tms ? TEST_LOGIC_RESET : CAPTURE_IR;
+            CAPTURE_IR: begin
+                state <= tms ? EXIT1_IR : SHIFT_IR;
+                ir <= 6'b0000_01;
             end
+            SHIFT_IR: begin
+                state <= tms ? EXIT1_IR : SHIFT_IR;
+                ir <= {tdi, ir[5:1]};
+            end
+            EXIT1_IR: 
+                state <= tms ? UPDATE_IR : PAUSE_IR;
+            PAUSE_IR: 
+                state <= tms ? EXIT2_IR : PAUSE_IR;
+            EXIT2_IR: 
+                state <= tms ? UPDATE_IR : SHIFT_IR;
+            UPDATE_IR: 
+                state <= tms ? SELECT_DR_SCAN : RUN_TEST_IDLE;
+            default: 
+                state <= TEST_LOGIC_RESET;
         endcase
-    end
-
-always_ff @(posedge tclk, negedge trst)
-    if (!trst)
-        state <= TEST_LOGIC_RESET;
-    else
-        state <= next_state;
-
-always_comb
-    if (!trst)
-        next_state = TEST_LOGIC_RESET;
-    else case(state)
-        TEST_LOGIC_RESET: 
-            next_state = tms ? TEST_LOGIC_RESET : RUN_TEST_IDLE;            
-        RUN_TEST_IDLE: 
-            next_state = tms ? SELECT_DR_SCAN : RUN_TEST_IDLE;            
-        SELECT_DR_SCAN: 
-            next_state = tms ? SELECT_IR_SCAN : CAPTURE_DR;         
-        CAPTURE_DR: 
-            next_state = tms ? EXIT1_DR : SHIFT_DR;     
-        SHIFT_DR: 
-            next_state = tms ? EXIT1_DR : SHIFT_DR;
-        EXIT1_DR: 
-            next_state = tms ? UPDATE_DR : PAUSE_DR;            
-        PAUSE_DR: 
-            next_state = tms ? EXIT2_DR : PAUSE_DR;
-        EXIT2_DR: 
-            next_state = tms ? UPDATE_DR : SHIFT_DR;
-        UPDATE_DR: 
-            next_state = tms ? SELECT_DR_SCAN : RUN_TEST_IDLE;
-        SELECT_IR_SCAN: 
-            next_state = tms ? TEST_LOGIC_RESET : CAPTURE_IR;
-        CAPTURE_IR: 
-            next_state = tms ? EXIT1_IR : SHIFT_IR;
-        SHIFT_IR: 
-            next_state = tms ? EXIT1_IR : SHIFT_IR;
-        EXIT1_IR: 
-            next_state = tms ? UPDATE_IR : PAUSE_IR;
-        PAUSE_IR: 
-            next_state = tms ? EXIT2_IR : PAUSE_IR;
-        EXIT2_IR: 
-            next_state = tms ? UPDATE_IR : SHIFT_IR;
-        UPDATE_IR: 
-            next_state = tms ? SELECT_DR_SCAN : RUN_TEST_IDLE;
-        default: 
-            next_state = TEST_LOGIC_RESET;
-    endcase
 
 endmodule
